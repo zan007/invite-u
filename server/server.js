eca2845a272283e77ada30cb5ff78696a7576743
zan007 <pploszaj91@gmail.com>
2019-01-12 12:41:20 +0100

    add statics to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use("/", express.static(__dirname));
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
